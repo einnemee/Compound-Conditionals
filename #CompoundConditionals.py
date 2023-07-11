49dabca1781b0be21f882f552a4813ce93c2bf94
Elizabeth Innemee <55023647+einnemee@users.noreply.github.com>
2023-07-11 20:24:54 +0100

    Create #CompoundConditionals.py

---
+++ #CompoundConditionals.py
@@ -0,0 +1,15 @@
+#Compound Conditionals
+
+annualSales = 200000
+newCustomer = True
+
+if annualSales >= 500000:
+    print("Gold Customer")
+elif annualSales >= 300000:
+    print("Silver Customer")
+elif annualSales >= 100000 and newCustomer == True:
+    print ("Bronze Customer and First-time Prize Winner")
+elif annualSales >= 100000:
+    print("Bronze Customer")
+    
+print("Thank you for your business")
